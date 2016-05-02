373dfed86d4b95312184b8bedc57a0a3971c7263
cylnclk <ceylan3313@homail.com>
2016-05-02 15:26:30 +0300

    hesaplamalar.cs'yi ekledim

---
+++ Odev-1/hesaplamalar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13253013HW1
+{
+    class hesaplamalar
+    {
+        public float toplama(float sayi1, float sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public float cıkarma(float sayi1, float sayi2)
+        {
+            return sayi1 - sayi2;
+
+        }
+        public float bolme(float sayi1, float sayi2)
+        {
+            return sayi1 / sayi2;
+
+        }
+        public float carpma(float sayi1, float sayi2)
+        {
+            return sayi1 * sayi2;
+
+        }
+    }
+}
